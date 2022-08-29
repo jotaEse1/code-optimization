35c906724a4c52ecc326dc34cb3c6e652bc383ad
Joaquin <joacosanchez20@gmail.com>
2022-08-29 19:13:59 -0300

    insertionSort added to the repo

---
+++ SortAlgos/insertionSort.js
@@ -0,0 +1,27 @@
+// ------------------ INSERTION SORT ------------------
+
+// time complexity ---> WC O(N**2) / AvgC O(N**2) / BC O(N)
+// space complexity --> O(1)
+
+const insertionSort = array => {
+    for (let i = 1; i < array.length; i++){
+        const tempVariable = array[i];
+
+        for (let j = i - 1; j >= 0; j--) {
+            const prevValue = array[j];
+
+            if(prevValue > tempVariable) {
+                array[j + 1] = prevValue
+                array[j] = tempVariable
+            }else break   
+        }
+    }
+
+    console.log(array)
+    return array
+}
+
+
+insertionSort([5, 1, 4, 3, 2]) // 1, 2, 3, 4, 5
+insertionSort([4, 2, 7, 1, 3]) // 1, 2, 3, 4, 7
+insertionSort([4, 2, 7, 1, 3, 10, 6, 5, 12, 8]) // 1, 2, 3, 4, 5, 6, 7, 8, 10, 12
